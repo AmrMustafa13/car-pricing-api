8a7e57a0dab861042a70399322ec1c517e5436cf
AmrMustafa13 <amrmostafa1311@gmail.com>
2024-02-13 14:16:53 +0200

    add user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  create(body: CreateUserDto) {
+    const user = this.userRepository.create(body);
+    return this.userRepository.save(user);
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.userRepository.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<UpdateUserDto>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    Object.assign(user, attrs);
+
+    return this.userRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.userRepository.remove(user);
+  }
+}
