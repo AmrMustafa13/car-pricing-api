8c854ba3d1c6ce72d2abb439fb2b7c15a31bf0ab
AmrMustafa13 <amrmostafa1311@gmail.com>
2024-02-15 14:02:38 +0200

    add create report service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Report } from './report.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private reportRepository: Repository<Report>,
+  ) {}
+
+  create(body: CreateReportDto, user: User) {
+    const report = this.reportRepository.create(body);
+    report.user = user;
+    return this.reportRepository.save(report);
+  }
+}
